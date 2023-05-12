baf6d409bb74f13f1771ba879a78bc597f1beebf
Ace_Leo <126467739+cangleiling@users.noreply.github.com>
2023-05-12 09:18:39 +0800

    Update city_name.py

--- 简易 12306/city_name.py
+++ 简易 12306/city_name.py
@@ -10,12 +10,6 @@ resp = requests.get(url, headers= headers)
 resp.encoding='utf-8'
 stations = re.findall('([\u4e00-\u9fa5]+)\|([A-Z]+)',resp.text)
 station = dict(stations)
-# print(station)
-# # station_d = {}
-# # for item in station:
-# #     station_d[station[item]] = item
-# # print(station_d)
-#               #在dumps时 如果文件中有中文汉字需要添加ensure_ascii= False
 json_data = json.dumps(station,ensure_ascii=False)
 with open('city.json','w',encoding='utf-8')as fp:
     fp.write(json_data)
